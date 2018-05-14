c18747ea2985bf78aead5f17d3fd828b558c83ef
savyounts@gmail.com <savyounts@gmail.com>
2018-05-14 12:11:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens.push
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name)
+  return kittens.unshift
+}
+
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function appendKitten(name){
+  [kittens, ...name]
+  return [kittens, ...name]
+}
