b30b626cb0c655527b1da2981130e9fd9fd290f5
savyounts@gmail.com <savyounts@gmail.com>
2018-05-14 12:17:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function destructivelyRemoveFirstKitten(){
 }
 
 function appendKitten(name){
-  [kittens, ...name]
-  return [kittens, ...name]
+  [...kittens, name]
+  return [...kittens, name]
 }
